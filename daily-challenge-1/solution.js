2af7a102fe9f8fa5b5e1b78681f8695dc1107452
Temi <karatemitayo@gmail.com>
2019-02-12 21:37:21 +0100

    modified script

--- daily-challenge-1/solution.js
+++ daily-challenge-1/solution.js
@@ -19,3 +19,34 @@ Sample Data: dog Expected Output: ["d", "do", "dog", "o", "og", "g"]
 
 Note all functions should be in the same Javascript file. Goodluck.
  */
+
+const library = [
+    {
+        author: 'Bill Gates', 
+        title: 'The Road Ahead', 
+        readingStatus: true 
+    }, 
+    { 
+        author: 'Steve Jobs',
+        title: 'Walter Isaacson',
+        readingStatus: true 
+    }, 
+    { 
+        author: 'Suzanne Collins', 
+        title: 'Mockingjay: The Final Book of The Hunger Games', 
+        readingStatus: false
+    } 
+];
+
+class StateLib {
+
+    readStatus(theArray){
+        theArray.forEach(function(books, index){
+            console.log(`Book ${++index}. ${books.readingStatus}`);
+        });
+    }
+
+
+}//End of Class
+let stateLib = new StateLib();
+stateLib.readStatus(library);
