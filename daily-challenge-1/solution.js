6871cf4002f8dce6bac615356dd1a9467efdd65e
Temi <karatemitayo@gmail.com>
2019-02-12 20:00:14 +0100

    created solution.js file

---
+++ daily-challenge-1/solution.js
@@ -0,0 +1,21 @@
+/**Solve the following challenges
+
+Deadline: 11pm 12-02-2019
+
+Create a file called solution.js and add your functions to it. 
+Commit this file, push to your repo and submit a pr.
+
+Write a JavaScript program to display the reading status 
+(i.e. display book name, author name and reading status) of the following books.
+
+var library =
+[ {author: 'Bill Gates', title: 'The Road Ahead', readingStatus: true }, 
+{ author: 'Steve Jobs', title: 'Walter Isaacson', readingStatus: true }, 
+{ author: 'Suzanne Collins', title: 'Mockingjay: The Final Book of The Hunger Games', 
+readingStatus: false } ];
+
+Write a JavaScript program which returns a subset of a string.
+Sample Data: dog Expected Output: ["d", "do", "dog", "o", "og", "g"]
+
+Note all functions should be in the same Javascript file. Goodluck.
+ */
