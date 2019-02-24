95384fecb277629ddc08562b155165612eb82a31
Moronke <moronkealiu02@gmail.com>
2019-02-24 14:04:53 +0100

    DailyChallenge(24-02-2019)

---
+++ vowel_square.js
@@ -0,0 +1,18 @@
+# DaiilyChallenge(24-02-2019)
+
+function VowelSquare(strArr) {
+    var vowels = 'aeiou'.split('');
+    for (let i = 0; i < strArr.length-1; i++) {
+        for (let j =0; j < strArr[i].length-1; j++) {
+            if (vowels.indexOf(strArr[i][j]) !=-1 && vowels.indexOf(strArr[i][j+1]) !=-1 && vowels.indexOf(strArr[i+1][j]) !=-1 && vowels.indexOf(strArr[i+1][j+1] !=-1)) {
+                return `${i} - ${j}`;
+        
+            }
+        }
+    }
+    return 'not found';
+}
+console.log(VowelSquare(["aqrst", "ukaei", "ffooo"])); 
+console.log(VowelSquare(["gg", "ff"])); 
+console.log(VowelSquare(["abcd", "eikr", "oufj"])); 
+console.log(VowelSquare(["aliu","tola","dapo","joye"]))
